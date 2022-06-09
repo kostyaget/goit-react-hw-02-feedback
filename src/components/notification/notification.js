6aaa0742d62694ceb2e1d9c1b98dc40186f91e0b
kostyaget <progogyd@gmail.com>
2022-06-09 17:16:22 +0300

    Notification

--- src/components/notification/notification.js
+++ src/components/notification/notification.js
@@ -1,1 +1,10 @@
-// import s from "."
+import s from "./notification.module.css";
+import propTypes from 'prop-types'
+
+export default function Notification({ title }) {
+    return <p className={s.header}>{title}</p>
+}
+
+Notification.propTypes = {
+    title: propTypes.string,
+};
