ba3d09b8bfb1c6dcd390fbf4ec49f57ca6092495
kostyaget <progogyd@gmail.com>
2022-06-09 17:07:16 +0300

    App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,58 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import Statistics from './statistics';
+import FeedbackOpt from './feedbackOpt';
+import Notification from './notification';
+import Section from './section';
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  onIncrement = e => {
+    const { id } = e.target;
+    this.setState(prevState => {
+      return { [id]: prevState[id] + 1 }
+    })
+  }
+
+  countTotalFeedback = () => {
+    return this.state.good + this.state.neutral + this.state.bad;
+  }
+
+  countPositiveFeedbackPercentage = () => {
+    return (
+      (this.state.good /
+        (this.state.good + this.state.neutral + this.state.bad)) *
+      100
+    )
+  }
+
+  render() {
+    const total = this.countTotalFeedback();
+    const positivePercentage = Math.floor(this.countPositiveFeedbackPercentage());
+    const eventHandler = this.onIncrement;
+
+    return (
+      <Section title="Please leave feedback">
+        <FeedbackOpt options={['good', 'neutral', 'bad']} onLeaveFeedback={eventHandler} />
+        {!total ? (
+          <Notification title = "There is on Feedback" />
+                  ) : (
+            <Statistics
+              good={this.state.good}
+              neutral={this.state.neutral}
+              bad={this.state.bad}
+              total={total}
+              positivePercentage={positivePercentage}
+          /> 
+        ) }
+      </Section>
+    )
+  }
+}
+
+export default App
+
